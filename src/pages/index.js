1857ca1b4b876130405d42c6051cefe60ec3e21b
Daviniel Nascimento <eng.daviniel@gmail.com>
2023-11-15 17:56:53 -0300

    adicionado o styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
+import Arrow from '../assets/icon-arrow.svg';
+import { Container, SearchSection, SearchInfos, MapContainer } from '../styles/HomeStyles';
+
 export default function Home() {
   return (
     <Container>
@@ -8,7 +11,38 @@ export default function Home() {
           <input type="text" placeholder="Search for any IP address or domain"></input>
           <button><Arrow /></button>
         </div>
+
+        <SearchInfos>
+          <ul>
+            <li>
+              <div>
+                <strong>IP Address</strong>
+                <p>192.199.0.100</p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <strong>Location</strong>
+                <p>Brooklyn, NY<br /> 10001</p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <strong>Timezone</strong>
+                <p>UTC -05:00</p>
+              </div>
+            </li>
+            <li>
+              <div>
+                <strong>ISP</strong>
+                <p>SpaceX Starlink</p>
+              </div>
+            </li>
+          </ul>
+        </SearchInfos>
       </SearchSection>
+
+      <MapContainer />
     </Container>
   )
 }
